ad953a0d4fdffc5a82343532ffebad4e02efdd03
Sebastian Hoß <seb@xn--ho-hia.de>
2025-01-14 04:10:32 +0100

    add VersionAddendum & re-generate
    Signed-off-by: Sebastian Hoß <seb@xn--ho-hia.de>

---
+++ bindings/swift/TreeSitterSshClientConfig/ssh_client_config.h
@@ -0,0 +1,16 @@
+#ifndef TREE_SITTER_SSH_CLIENT_CONFIG_H_
+#define TREE_SITTER_SSH_CLIENT_CONFIG_H_
+
+typedef struct TSLanguage TSLanguage;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+const TSLanguage *tree_sitter_ssh_client_config(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // TREE_SITTER_SSH_CLIENT_CONFIG_H_
